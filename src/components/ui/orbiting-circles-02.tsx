63b9c8a069f9dd0d88ea15169350910e60aad5c7
jaztulsi <jaztulsi99@gmail.com>
2026-07-30 21:52:59 -0700

    Revert sponsor globe to original layout

--- src/components/ui/orbiting-circles-02.tsx
+++ src/components/ui/orbiting-circles-02.tsx
@@ -5,12 +5,14 @@ import ParticleSphereAnimation from "@/components/ui/orbiting-circles-02-utils/p
 
 const orbits = [
   {
-    size: "w-[min(58vw,224px)] h-[min(58vw,224px)] lg:w-180 lg:h-180",
+    size: "w-110 h-110 md:w-180 md:h-180",
     duration: 18,
-    icons: [{ src: "/context66-logo.png", alt: "Context66", angle: 0, plate: true }],
+    icons: [
+      { src: "/context66-logo.png", alt: "Context66", angle: 0, plate: true },
+    ],
   },
   {
-    size: "w-[min(68vw,272px)] h-[min(68vw,272px)] lg:w-220 lg:h-220",
+    size: "w-150 h-150 md:w-220 md:h-220",
     duration: 24,
     icons: [
       { src: "/yri-science-logo.png", alt: "YRI Science", angle: 0, plate: true },
@@ -18,7 +20,7 @@ const orbits = [
     ],
   },
   {
-    size: "w-[min(86vw,320px)] h-[min(86vw,320px)] lg:w-265 lg:h-265",
+    size: "w-180 h-180 md:w-265 md:h-265",
     duration: 30,
     icons: [
       { src: "/pcbway-logo.png", alt: "PCBWay", angle: 45, plate: true },
@@ -29,7 +31,7 @@ const orbits = [
 
 export default function OrbitingCirclesGlobeDemo() {
   return (
-    <div className="relative w-full h-[min(52vw,224px)] lg:h-160 overflow-hidden flex justify-center">
+    <div className="relative w-full h-110 md:h-160 overflow-hidden flex justify-center">
       <style>{`
         @keyframes orbit-cw {
           from { transform: rotate(var(--start-angle)) }
@@ -50,7 +52,7 @@ export default function OrbitingCirclesGlobeDemo() {
       `}</style>
 
       {/* Center particle globe */}
-      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 aspect-square pointer-events-none w-[min(42vw,176px)] lg:w-145 z-10">
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 aspect-square pointer-events-none w-75 md:w-145 z-10">
         <ParticleSphereAnimation />
       </div>
 
@@ -77,7 +79,7 @@ export default function OrbitingCirclesGlobeDemo() {
             {allIcons.map((iconData, iconIndex) => (
               <div
                 key={iconIndex}
-                className="absolute top-0 left-1/2 h-1/2 -ml-6 md:-ml-8 origin-bottom flex flex-col justify-start items-center"
+                className="absolute top-0 left-1/2 h-1/2 -ml-8 origin-bottom flex flex-col justify-start items-center"
                 style={
                   {
                     "--start-angle": `${iconData.angle}deg`,
@@ -86,7 +88,7 @@ export default function OrbitingCirclesGlobeDemo() {
                 }
               >
                 <div
-                  className={`p-3 sm:p-4 border border-border rounded-full -mt-6 md:-mt-8 relative z-10 ${iconData.plate ? "bg-white/95" : "bg-background"}`}
+                  className={`p-3 sm:p-4 border border-border rounded-full -mt-8 relative z-10 ${iconData.plate ? "bg-white/95" : "bg-background"}`}
                   style={
                     {
                       "--counter-offset": `${-iconData.angle}deg`,
